53d65d340813a325d4d7700cd68e2fb745e1efdc
John Brinkman <john@brinko.com>
2015-02-25 17:54:20 -0800

    adding comment argument

--- platform/tag_for_release.sh
+++ platform/tag_for_release.sh
@@ -1,15 +1,16 @@
 #!/bin/bash - 
 # Assume this is executed from <root>/platform, so the project root is $pwd/..
-EXPECTED_ARGS=1
+EXPECTED_ARGS=2
 E_BADARGS=65
 
 if [ $# -ne $EXPECTED_ARGS ]
 then
-  echo "Usage: `basename $0` {tag}"
+  echo "Usage: `basename $0` {tag} {comment}"
   exit $E_BADARGS
 fi
 
 ver_str=$1
+comment=$2
 is_dirty=$(git status -s --porcelain | wc -l)
 
 if [ $is_dirty -gt 1 ]
@@ -20,7 +21,7 @@ else
   python inc_version.py -s $ver_str -f ../spl/main.cpp -v kDcVersionString
   python inc_version.py -f ./osx/Info.plist -v string -s $ver_str
   git commit -a -m"version bump to $ver_str"
-  git tag -a $ver_str -m"version $ver_str"
+  git tag -a $ver_str -m"$comment"
 fi
 
 
