092e3be122049fc275fc3a0b4af7b90a2bc8943d
John Brinkman <johnb@strymon.net>
2015-02-26 08:34:36 -0800

    version bump to 0.9.4.9

--- platform/tag_tool.sh
+++ platform/tag_tool.sh
@@ -1,17 +1,16 @@
 #!/bin/bash - 
 # Assume this is executed from <root>/platform, so the project root is $pwd/..
-EXPECTED_ARGS=2
+EXPECTED_ARGS=1
 E_BADARGS=65
 
 if [ $# -ne $EXPECTED_ARGS ]
 then
-  echo "Usage: `basename $0` {tag} {comment}"
+  echo "Usage: `basename $0` {tag}"
   exit $E_BADARGS
 fi
 
 ver_str=$1
-comment=$2
-is_dirty=$(git status -s --porcelain | wc -l)
+is_dirty=$(git status -s -u no --porcelain | wc -l)
 
 if [ $is_dirty -gt 1 ]
 then
@@ -21,7 +20,7 @@ else
   python inc_version.py -s $ver_str -f ../spl/main.cpp -v kDcVersionString
   python inc_version.py -f ./osx/Info.plist -v string -s $ver_str
   git commit -a -m"version bump to $ver_str"
-  git tag -a $ver_str -m"$comment"
+  git tag -a $ver_str -m"version $ver_str"
 fi
 
 
--- spl/main.cpp
+++ spl/main.cpp
@@ -22,7 +22,7 @@
 #include "DcConArgs.h"
 
 
-static const char* kDcVersionString = "0.9.4.8";
+static const char* kDcVersionString = "0.9.4.9";
 
 
 void muteMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
