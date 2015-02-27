be060589c2c3ae61b358fa8d8c015cfd53902288
John Brinkman <john@brinko.com>
2015-02-26 20:28:49 -0800

    Version bump to 0.9.4.10

--- spl/main.cpp
+++ spl/main.cpp
@@ -22,7 +22,7 @@
 #include "DcConArgs.h"
 
 
-static const char* kDcVersionString = "0.9.4.9";
+static const char* kDcVersionString = "0.9.4.10";
 
 
 void muteMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
