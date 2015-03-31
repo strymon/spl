c6686dd9ada2b40dadbc0cf5c6a627a0522ef3c7
John Brinkman <john@brinko.com>
2015-03-31 13:50:24 -0700

    verson bump

--- spl/main.cpp
+++ spl/main.cpp
@@ -22,7 +22,7 @@
 #include "DcConArgs.h"
 
 
-static const char* kDcVersionString = "0.9.4.42";
+static const char* kDcVersionString = "0.9.4.43";
 
 
 void muteMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
